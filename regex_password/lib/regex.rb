86f7b2b3ff51f7152df4c0cb557c62496f9051c1
Paula <pmuldoon@umich.edu>
2017-04-07 21:26:32 +0100

    6 plus character count in regex password

--- regex_password/lib/regex.rb
+++ regex_password/lib/regex.rb
@@ -1,8 +1,19 @@
 def check(password)
-  regex = /[A-Z][a-z]/
+  regex = /\A(?=\w{6,}\z)/
+    if password =~ regex
+    true
+  else
+    false
+  end
+end
+
+=begin
+def check(password)
+  regex = /[A-Z][a-z])^([0-9])/
   if password =~ regex
     true
   else
     false
   end
 end
+=end
--- regex_password/spec/regex_spec.rb
+++ regex_password/spec/regex_spec.rb
@@ -1,7 +1,11 @@
 require "regex"
 describe "regex" do
+=begin
   it "checks whether an expression contains at least one uppercase character" do
     expect(check("Cambridge4321")).to eq true
+  end
+  it "checks whether an expression contains at least one uppercase character" do
+    expect(check("cAmbridge4321")).to eq true
   end
   it "checks whether an expression contains at least one uppercase character" do
     expect(check("cambridge4321")).to eq false
@@ -11,5 +15,13 @@ describe "regex" do
   end
   it "checks whether an expression contains at least one lowercase character" do
     expect(check("CAMBRIDGE321")).to eq false
+  end
+
+  it "checks whether an expression contains at least one digit" do
+    expect(check("Cambridge4321")).to eq true
+  end
+=end
+  it "checks whether the password has at least six characters" do
+    expect(check("Cambridge4321")).to eq true
   end
 end
